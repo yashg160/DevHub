b11e4f488336fc0d53afada9a00df4bbb1db9624
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-07 15:37:31 +0530

    Token serializabilty and permission issue on Github

--- users/views.py
+++ users/views.py
@@ -1,19 +1,20 @@
 from rest_framework import permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import get_user_model
 import requests
 from json import dumps, loads
 
 @api_view(['GET'])
+@permission_classes([permissions.AllowAny])
 def authorize_user(request, code):
     params = {
         "client_id" : "e97710fdd921e6d456bd",
         "client_secret" : "47bb9addd9b01fc4b35aa4cad29b0d7406c2ac35",
         "code" : code
     }
-      
+    
     access_token = requests.post('https://github.com/login/oauth/access_token', params = params).text
     
     user_data = requests.get(
@@ -24,13 +25,15 @@ def authorize_user(request, code):
     )
     data = loads(user_data.text)
     User = get_user_model()
-    user, created_user = User.objects.get_or_create(username = user_data.login)
+    user, created_user = User.objects.get_or_create(
+        username = data['login'], 
+        extra_detail_url = data['url']
+    )
 
-    token, created = Token.objects.get_or_create(user = user)
-    print(created_user, token)
+    token, _ = Token.objects.get_or_create(user = user)
     
     return Response({
         "data" : data,
         "first_login" : created_user,
-        "token" : token
+        "token" : token.key
     })
