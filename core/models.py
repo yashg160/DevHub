363e9e041e08c823066d6463fea89ad9745bea5e
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-14 15:10:09 +0530

    Added genre field to Question

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=84)
     subscribers = models.ManyToManyField(CustomUser, related_name="subscribed_genres")
 
-    def __str__(self) :
+    def __str__(self):
         return self.name
 
 class Topic(models.Model):
@@ -20,6 +20,7 @@ class Question(models.Model):
     question = models.CharField(max_length = 128)
     asker = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name="asked_questions")
     requested = models.ManyToManyField(CustomUser, related_name="answer_requests")
+    genres = models.ManyToManyField(Genre, related_name = "questions")
     followers = models.ManyToManyField(CustomUser, related_name="followed_questions")
     topics = models.ManyToManyField(Topic, related_name="questions")
     url = models.CharField(max_length = 150)
@@ -34,6 +35,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE, related_name="answers")
     upvoters = models.ManyToManyField(CustomUser, related_name="upvoted_answers")
     created_at = models.DateTimeField(auto_now_add = True, blank = True)
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length = 200)
