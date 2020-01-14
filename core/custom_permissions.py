e6743371a0aa0d79c146522a25e134676f248953
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-14 15:05:15 +0530

    Custom permission modification

--- core/custom_permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class isSuperuserOrReadOnly(permissions.BasePermission):
-    def has_object_permissions(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        return obj.user.is_superuser == True
