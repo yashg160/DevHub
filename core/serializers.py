b0c6ea933c4cfb09f9069bbb2d460d10f97c9def
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-20 23:09:35 +0530

    Added View for Question management

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from rest_framework import serializers
-from core.models import Genre, Topic, Question
-
+from core.models import Genre, Topic, Question, Answer
+from users.models import CustomUser
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
@@ -25,16 +25,54 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ('question', 'asker', 'requested', 'genres', 'followers', 'topics', 'url',
                   'created_at', 'answers', )
 
-    def create(self, validated_data):
-        question = Question.objects.create(**validated_data)
+    # pylint: disable=W0221
+    def create(self, validated_data, **kwargs):
+        current_user = kwargs['current_user']
+        question = Question.objects.create(
+            question = validated_data.get('question'),
+            asker = current_user
+        )
+        question.followers.add(current_user)
         question.url = question.question.lower().replace(' ', '-')
         question.save()
         return question
 
-    def update(self, instance, validated_data):
+    # pylint: disable=W0221
+    def update(self, instance, validated_data, **kwargs):
         new_question = validated_data.get('question', instance.question)
+        requested = validated_data.get('requested', [])
+        followed = validated_data.get('followed', False)
+        unfollowed = validated_data.get('unfollowed', False)
+        current_user = kwargs['current_user']
+        if followed:
+            instance.followers.add(current_user)
+
+        if unfollowed:
+            instance.followers.remove(current_user)
+
+        if current_user == instance.asker :
+            for username in requested :
+                user = CustomUser.objects.get(username = username)
+                if user != instance.asker and user not in instance.requested.all():
+                    instance.requested.add(user)
+
         instance.question = new_question
         instance.url = new_question.lower().replace(' ', '-')
         instance.updated_at = datetime.now()
         instance.save()
         return instance
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('answer', 'author', 'question.question', 'upvoters', 'created_at', )
+
+    # pylint: disable=W0221
+    def create(self, validated_data, **kwargs):
+        answer = Answer.objects.create(
+            answer = validated_data.get('answer'),
+            author = kwargs['current_user'],
+            question = Question.objects.get(url = validated_data.get('url'))
+        )
+        answer.save()
+        return answer
--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
-# pylint: disable=W0702,C0103
 from django.db.models import Count, Subquery, OuterRef
 
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, authentication_classes
 
+import core
 from core.utils.pagination import SmallResultSetPagination
 from core.utils.permissions import isSuperuserOrReadOnly
-from core.serializers import GenreSerializer
+from core.serializers import GenreSerializer, QuestionSerializer
 from core.models import Genre, Question, Answer
 # from django.contrib.auth import get_user_model
 
@@ -54,7 +55,6 @@ class GenresView(APIView):
 def subscribe_genres(request):
 
     genres = request.data.get('genres').split(',')
-    print(genres)
     genre_objects = []
     for genre_name in genres:
         try:
@@ -81,13 +81,59 @@ def HomePageView(request):
 
     answer_subquery = Answer.objects.filter(
         question=OuterRef('pk')
-    ).annonate(
-        upvotes=Count('upvoters')
-    ).order_by('-upvotes')
+        ).annotate(
+            upvotes=Count('upvoters')
+        ).order_by('-upvotes')
 
-    questions = Question.objects.annonate(
-        answer=Subquery(answer_subquery.values('answer')[-1])
-    ).filter(genre__in = subscribed_genres)
+    questions = Question.objects.annotate(
+        answer=Subquery(answer_subquery.values('answer')[:1])
+        ).filter(genres__in = subscribed_genres.all())
 
     result_page = paginator.paginate_queryset(questions, request)
     return paginator.get_paginated_response(result_page)
+
+class QuestionView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+
+    def update(self, request, url):
+        try:
+            question = Question.objects.get(url = url)
+        except core.models.Question.DoesNotExist:
+            return Response({'status' : 'error', 'message' : 'Question does not exist'})
+        updated_question = request.data.get('question')
+        serializer = QuestionSerializer(question, updated_question, current_user=request.user)
+        if serializer.is_valid():
+            serializer.save()
+        else :
+            return Response({'status' : 'error', 'message' : serializer.errors})
+
+        return Response({
+            'status' : 'success',
+            'message' : 'Question updated succesfully',
+            'url' : serializer.data
+        })
+
+    def get(self, request, url):
+        try:
+            question = Question.objects.get(url = url)
+        except core.models.Question.DoesNotExist :
+            return Response({'status' : 'error', 'message' : 'Question does not exist'})
+        serialized = QuestionSerializer(question)
+        return Response({'status' : 'success', 'data' : serialized.data})
+
+    def post(self, request):
+        try :
+            question = request.data.get('question')
+        except:
+            return Response({'status' : 'error', 'message' : 'Missing question in request body'})
+
+        serializer = QuestionSerializer(question, current_user = request.user)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response({'status' : 'error', 'message' : serializer.errors})
+        return Response({
+            'status' : 'success',
+            'message' : 'Question added succesfully'
+        })
