8380763cefa25e275e14351f2ae36b7a6d6ce3a5
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-20 21:02:16 +0530

    New serializer for Question

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from rest_framework import serializers
-from core.models import Genre, Topic
+from core.models import Genre, Topic, Question
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +17,24 @@ class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = ('name', 'category', )
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question', 'asker', 'requested', 'genres', 'followers', 'topics', 'url',
+                  'created_at', 'answers', )
+
+    def create(self, validated_data):
+        question = Question.objects.create(**validated_data)
+        question.url = question.question.lower().replace(' ', '-')
+        question.save()
+        return question
+
+    def update(self, instance, validated_data):
+        new_question = validated_data.get('question', instance.question)
+        instance.question = new_question
+        instance.url = new_question.lower().replace(' ', '-')
+        instance.updated_at = datetime.now()
+        instance.save()
+        return instance
