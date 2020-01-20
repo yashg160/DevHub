dee1de99b25161931bd952eb9728d12f710d364b
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-20 21:02:26 +0530

    HomePage API added

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from core.views import GenresView, subscribe_genres
+from django.urls import path
+from core.views import GenresView, subscribe_genres, HomePageView
 
 urlpatterns = [
     path('genre', GenresView.as_view()),
-    path('genres/subscribe', subscribe_genres)
+    path('genres/subscribe', subscribe_genres),
+    path('home', HomePageView),
 ]
