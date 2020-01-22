8e4f6da96801157c77731fe233f4df50e4f7e64b
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2020-01-22 20:00:54 +0530

    New URLS s for Question and Answers

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from core.views import GenresView, subscribe_genres, HomePageView
+from core.views import (
+    GenresView, subscribe_genres, HomePageView, QuestionView, AnswerView, 
+    QuestionCreateView, AnswerCreateView
+)
+
 
 urlpatterns = [
     path('genre', GenresView.as_view()),
     path('genres/subscribe', subscribe_genres),
     path('home', HomePageView),
+    path('questions', QuestionCreateView.as_view()),
+    path('questions/<str:url>', QuestionView.as_view()),
+    path('answers', AnswerCreateView.as_view()),
+    path('answers/<str:answer_id>', AnswerView.as_view()),
 ]
