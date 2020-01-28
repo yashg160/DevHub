8e4a6200da0cb03afdac75619ad8c336a7ff192f
Yash Gupta <yashg160@gmail.com>
2020-01-28 19:52:36 +0530

    Added Navbar to Dashboard

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,38 +1,45 @@
 import React from 'react';
 
 import Backdrop from '@material-ui/core/Backdrop';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 import serverUrl from '../config';
 
 import Cookies from 'js-cookie';
 
-
 export default class Dashboard extends React.Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            user: null,
-            loading: true
-        }
-    }    
+		this.state = {
+			user: null,
+			loading: true,
+			error: false
+		};
+	}
 
-    componentDidMount() {
+	componentDidMount() {
+		var token = Cookies.get('TOKEN');
+		console.log(token);
+		this.setState({ loading: false });
+	}
+	render() {
+		if (this.state.loading)
+			return <Backdrop color='#fff' open={this.state.loading} />;
+		else if (this.state.error) return <h1>There was an error</h1>;
 
-        var token = Cookies.get('TOKEN');
-        console.log(token);
-        this.setState({ loading: false });
-        
-    }
-    render() {
-        if (this.state.loading)
-            return (
-                <Backdrop color='#fff' open={this.state.loading}/>
-            )
-        
-        return (
-            <h1>YO! CHECK THIS OUT YA ALL!</h1>
-        )
-    }
+		return (
+			<div>
+				<AppBar position='static'>
+					<Toolbar>
+						<Typography variant='h6'>Reactora</Typography>
+					</Toolbar>
+				</AppBar>
+				<Container maxWidth='md'></Container>
+			</div>
+		);
+	}
 }
