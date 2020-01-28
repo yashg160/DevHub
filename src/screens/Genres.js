6f3c3b9e5d9b8b52dbc0d6772af54ddf18b05576
Yash Gupta <yashg160@gmail.com>
2020-01-28 19:09:32 +0530

    Genre Page Logic Complete

--- src/screens/Genres.js
+++ src/screens/Genres.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import Backdrop from '@material-ui/core/Backdrop';
 import AppBar from '@material-ui/core/AppBar';
@@ -11,6 +12,7 @@ import serverUrl from '../config';
 
 import Cookies from 'js-cookie';
 import { Container } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 export default class Genres extends React.Component {
 	constructor(props) {
@@ -18,6 +20,8 @@ export default class Genres extends React.Component {
 
 		this.state = {
 			loading: true,
+			error: false,
+			redirect: false,
 			content: null,
 			genreTouched: false
 		};
@@ -64,8 +68,61 @@ export default class Genres extends React.Component {
 		this.forceUpdate();
 	}
 
+	async subscribeToGenres(token, subscribedGenres) {
+		let rawResponse = await fetch(serverUrl + '/api/genres/subscribe', {
+			method: 'POST',
+			headers: {
+				Authorization: `Token ${token}`,
+				Accept: '*/*',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				genres: subscribedGenres
+			})
+		});
+
+		let res = await rawResponse.json();
+		if (res.status !== 'success') throw Error();
+		return res;
+	}
+
+	handleSubscribeClick() {
+		this.setState({ loading: true, error: false });
+
+		//Get the token from cookies
+		const token = Cookies.get('TOKEN');
+		console.log(token);
+
+		//Create the list of genres to be sent to backend that the user has subscribed to
+		let genres = [];
+		this.state.content.map(g => {
+			if (g[1]) genres.push(g[0]);
+		});
+		var subscribedGenres = genres.join(',');
+		console.log('Subscribed to genres: ', subscribedGenres);
+
+		this.subscribeToGenres(token, subscribedGenres)
+			.then(res => {
+				console.group(res);
+				this.setState({ loading: false, error: false, redirect: true });
+			})
+			.catch(error => {
+				console.error(error);
+				this.setState({ loading: false, error: true, redirect: false });
+			});
+	}
+
 	render() {
 		if (this.state.loading) return <Backdrop open={this.state.loading} />;
+		else if (this.state.error) return <h1>There was an error</h1>;
+		else if (this.state.redirect)
+			return (
+				<Redirect
+					to={{
+						pathname: '/dashboard'
+					}}
+				/>
+			);
 		return (
 			<div>
 				<AppBar position='static'>
@@ -99,6 +156,21 @@ export default class Genres extends React.Component {
 								onClick={() => this.handleGenreClick(i)}
 							/>
 						))}
+					</div>
+
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center'
+						}}>
+						<Button
+							variant='contained'
+							color='primary'
+							disabled={!this.state.genreTouched}
+							onClick={() => this.handleSubscribeClick()}>
+							SUBSCRIBE
+						</Button>
 					</div>
 				</Container>
 			</div>
