ba41516412a158774f56d4aa6ed996ebb0c1f71b
Yash Gupta <yashg160@gmail.com>
2020-01-29 18:24:59 +0530

    Basic Question screen added

--- src/screens/Question.js
+++ src/screens/Question.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+
+import serverUrl from '../config';
+import Backdrop from '@material-ui/core/Backdrop';
 
 export default class Question extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			loading: false,
+			error: false
+		};
+	}
+
+	async getQuestionData(token, questionUrl) {
+		let rawResponse = await fetch(
+			serverUrl + `/api/questions/${questionUrl}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Token ${token}`,
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+
+		let res = await rawResponse.json();
+
+		return res;
+	}
+
+	componentDidMount() {
+		var token = Cookies.get('TOKEN');
+		console.log(token);
+
+		var questionUrl = this.props.match.params.questionUrl;
+		console.log(questionUrl);
+
+		this.getQuestionData(token, questionUrl)
+			.then(res => {
+				console.group(res);
+
+				this.setState({ loading: false, error: false });
+			})
+			.catch(error => {
+				console.error(error);
+				this.setState({ loading: false, error: true });
+			});
+	}
+
 	render() {
-		return <h1>{this.props.history.location.pathname}</h1>;
+		if (this.state.loading)
+			return <Backdrop open={this.state.loading} color='#fff' />;
+
+		console.group(this.props.match.params.questionUrl);
+		return <h1>{this.props.match.params.questionUrl}</h1>;
 	}
 }
