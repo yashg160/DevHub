534a32f87d1c079b48917687d4a04bec76939b22
Yash Gupta <yashg160@gmail.com>
2020-02-01 10:57:04 +0530

    Tabs boilerplate added

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Tabs from './tabs/Tabs';
+import './styles/tabs.css';
 
 export default class Profile extends React.Component {
 	render() {
-		return <h1>This is profile screen</h1>;
+		return (
+			<div>
+				<h1>Tabs Here</h1>
+				<Tabs>
+					<div label='Gator'>
+						See ya later, <em>Alligator</em>!
+					</div>
+					<div label='Croc'>
+						After 'while, <em>Crocodile</em>!
+					</div>
+					<div label='Sarcosuchus'>
+						Nothing to see here, this tab is <em>extinct</em>!
+					</div>
+				</Tabs>
+			</div>
+		);
 	}
 }
