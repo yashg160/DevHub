bc7f8a7f2b5536ae0b72c244513cbd39881f8169
Yash Gupta <yashg160@gmail.com>
2020-02-02 14:27:59 +0530

    Styling and theming

--- src/screens/tabs/Answers.js
+++ src/screens/tabs/Answers.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-
-import Button from '@material-ui/core/Button';
-
 import Link from '@material-ui/core/Link';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import InfiniteScroll from 'react-infinite-scroll-component';
-
 import serverUrl from '../../config';
 import utils from '../../utils';
-
-import Cookies from 'js-cookie';
+import theme from '../../theme';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 export default class Answers extends React.Component {
 	constructor(props) {
@@ -23,84 +19,89 @@ export default class Answers extends React.Component {
 	}
 	render() {
 		return (
-			<InfiniteScroll
-				style={{ marginTop: '1rem' }}
-				dataLength={this.state.answers.length}
-				next={() => console.log('Fetch more questions')}
-				hasMore={false}
-				loader={
-					<div
-						style={{
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center'
-						}}>
-						<CircularProgress
-							color='secondary'
-							style={{ margin: '2rem' }}
-						/>
-					</div>
-				}
-				endMessage={
-					<Typography
-						variant='body1'
-						style={{ textAlign: 'center', marginTop: '1rem' }}>
-						That's all! Ask a few more questions.
-					</Typography>
-				}>
-				{this.state.answers.map(answer => (
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							padding: '1rem',
-							border: '0.1rem solid #bababa',
-							marginTop: '0.5rem',
-							marginBottom: '0.5rem'
-						}}>
-						<Typography
-							variant='h6'
+			<ThemeProvider theme={theme.theme}>
+				<InfiniteScroll
+					style={{ marginTop: '1rem' }}
+					dataLength={this.state.answers.length}
+					next={() => console.log('Fetch more questions')}
+					hasMore={false}
+					loader={
+						<div
 							style={{
-								fontWeight: 700,
-								textTransform: 'capitalize'
+								width: '100%',
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center'
 							}}>
-							{String(answer.question)
-								.split('-')
-								.join(' ')}
-						</Typography>
-
-						<Typography
-							variant='subtitle2'
-							style={{ marginTop: '1rem' }}>
-							You Answered:
-						</Typography>
+							<CircularProgress
+								color='secondary'
+								style={{ margin: '2rem' }}
+							/>
+						</div>
+					}
+					endMessage={
 						<Typography
 							variant='body1'
-							style={{ marginTop: '0.1rem' }}>
-							{answer.answer}
+							style={{ textAlign: 'center', marginTop: '1rem' }}>
+							That's all! Ask a few more questions.
 						</Typography>
+					}>
+					{this.state.answers.map(answer => (
 						<div
 							style={{
 								display: 'flex',
-								flexDirection: 'row',
-								marginTop: '0.5rem',
-								color: '#54e1e3',
-								verticalAlign: 'center'
+								flexDirection: 'column',
+								padding: '1rem',
+								border: '1px solid #eeeeee',
+								borderRadius: '4px',
+								marginBottom: '1rem'
 							}}>
-							<ThumbUpIcon />
+							<Typography
+								variant='h6'
+								style={{
+									fontWeight: 600,
+									textTransform: 'capitalize'
+								}}>
+								{String(answer.question)
+									.split('-')
+									.join(' ')}
+							</Typography>
+
+							<Typography
+								variant='subtitle2'
+								color='textSecondary'
+								style={{ marginTop: '1rem' }}>
+								You Answered:
+							</Typography>
 							<Typography
 								variant='body1'
-								style={{
-									fontWeight: 600
-								}}>
-								{' '}
-								&#183; {answer.upvoters.length}
+								style={{ marginTop: '0.1rem' }}>
+								{answer.answer}
 							</Typography>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'row',
+									alignItems: 'center',
+									marginTop: '0.5rem',
+									verticalAlign: 'center'
+								}}>
+								<ThumbUpIcon color='primary' />
+								<Typography
+									variant='body1'
+									color='primary'
+									style={{
+										fontWeight: 600,
+										marginTop: '4px',
+										marginLeft: '4px'
+									}}>
+									{answer.upvoters.length}
+								</Typography>
+							</div>
 						</div>
-					</div>
-				))}
-			</InfiniteScroll>
+					))}
+				</InfiniteScroll>
+			</ThemeProvider>
 		);
 	}
 }
