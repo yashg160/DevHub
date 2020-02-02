429fdcd0ab1ce2d368385eec31cec4f25ae46196
Yash Gupta <yashg160@gmail.com>
2020-02-02 21:59:02 +0530

    Added question modal

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -15,6 +15,9 @@ import theme from '../theme';
 import { ThemeProvider } from '@material-ui/core/styles/';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Modal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import TextField from '@material-ui/core/TextField';
 
 export default class Profile extends React.Component {
 	constructor(props) {
@@ -30,7 +33,10 @@ export default class Profile extends React.Component {
 			comments: null,
 			upvotedAnswers: null,
 			upvotedComments: null,
-			requests: null
+			requests: null,
+			questionModal: false,
+			newQuestion: '',
+			newQuestionError: false
 		};
 	}
 	async getProfileData(userName, token) {
@@ -131,7 +137,7 @@ export default class Profile extends React.Component {
 											color='secondary'
 											onClick={() =>
 												this.setState({
-													modalVisible: true
+													questionModal: true
 												})
 											}
 											style={{
@@ -346,6 +352,165 @@ export default class Profile extends React.Component {
 						Sign Out
 					</MenuItem>
 				</Menu>
+				<Modal
+					aria-labelledby='modal-question'
+					aria-describedby='modal-ask-question'
+					open={this.state.questionModal}
+					onClose={() => this.setState({ questionModal: false })}
+					closeAfterTransition
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+					BackdropComponent={Backdrop}
+					BackdropProps={{ timeout: 500 }}>
+					<Fade in={this.state.questionModal}>
+						<div
+							style={{
+								backgroundColor: '#fff',
+								width: '50%'
+							}}>
+							<div
+								style={{
+									backgroundColor: '#e3e3e3',
+									padding: '1rem'
+								}}>
+								<Typography variant='h6'>
+									Add Question
+								</Typography>
+							</div>
+
+							<div style={{ padding: '1rem' }}>
+								<div>
+									<Typography
+										variant='h6'
+										style={{
+											fontWeight: 700,
+											marginBottom: '0.5rem'
+										}}>
+										Tips on getting good answers quickly
+									</Typography>
+
+									<div style={{ marginTop: '1rem' }}>
+										<Typography
+											variant='body1'
+											style={{ marginTop: '0.5rem' }}>
+											Make sure your question hasn't been
+											asked already
+										</Typography>
+										<Typography
+											variant='body1'
+											style={{ marginTop: '0.5rem' }}>
+											Keep your question short and to the
+											point
+										</Typography>
+										<Typography
+											variant='body1'
+											style={{ marginTop: '0.5rem' }}>
+											Double-check grammar and spelling
+										</Typography>
+									</div>
+								</div>
+
+								<div
+									style={{
+										display: 'flex',
+										flexDirection: 'row',
+										justifyContent: 'flex-start',
+										marginTop: '1.5rem',
+										marginBottom: '1.5rem',
+										alignItems: 'center'
+									}}>
+									<Avatar
+										src={this.state.user.avatar_url}
+										alt={this.state.user.name}
+										style={{
+											height: '2rem',
+											width: '2rem'
+										}}
+									/>
+									<Typography
+										variant='body1'
+										style={{ marginLeft: '1rem' }}>
+										Posting as {this.state.user.name}
+									</Typography>
+								</div>
+
+								<div>
+									<Typography variant='subtitle1'>
+										User Name asked
+									</Typography>
+									<TextField
+										id='question'
+										label='Your Question'
+										InputProps={{
+											style: {
+												fontSize: 26,
+												fontWeight: 600
+											}
+										}}
+										placeholder={
+											'Start you question with "What," "Why," or "How."'
+										}
+										multiline
+										rowsMax='3'
+										fullWidth
+										value={this.state.newQuestion}
+										onChange={event =>
+											this.setState({
+												newQuestion: event.target.value
+											})
+										}
+										onFocus={() =>
+											this.setState({
+												newQuestionError: false
+											})
+										}
+										error={this.state.newQuestionError}
+										helperText={
+											this.state.newQuestionError
+												? 'Please check your question'
+												: ''
+										}
+									/>
+								</div>
+								<div
+									style={{
+										padding: '2rem',
+										display: 'flex',
+										flexDirection: 'row',
+										justifyContent: 'flex-end',
+										alignItems: 'center'
+									}}>
+									<Button
+										variant='text'
+										onClick={() =>
+											this.setState({
+												questionModal: false
+											})
+										}
+										style={{ marginRight: '0.2rem' }}>
+										Cancel
+									</Button>
+									<Button
+										color='primary'
+										variant='contained'
+										onClick={() =>
+											this.setState({
+												questionModal: false,
+												genresModal: true
+											})
+										}
+										style={{ marginLeft: '0.2rem' }}>
+										Add question
+									</Button>
+								</div>
+							</div>
+						</div>
+					</Fade>
+				</Modal>
 			</ThemeProvider>
 		);
 	}
