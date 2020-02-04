ac1162e820722e764b261b80c12ef926829c7b22
Yash Gupta <yashg160@gmail.com>
2020-02-04 21:50:31 +0530

    Created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import PropTypes from 'prop-types';
+import '../screens/styles/dashboard.css';
+
+// Define the props types and their values. 
+Navbar.propTypes = {
+    screenName: PropTypes.string,
+    handleAvatarClick: PropTypes.func,
+    handleAddQuestionClick: PropTypes.func
+}
+
+// Screen Name controls whether the add question button is rendered or not. On genres screen, the button is not rendered.
+
+export default class Navbar extends React.PureComponent {
+
+    render() {
+        var { handleAvatarClick, handleAddQuestionClick } = this.props;
+        return (
+            <AppBar position='fixed'>
+                <Toolbar variant='regular' color='primary'>
+                    <Container maxWidth='lg'>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexGrow: 1,
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'space-between'
+                            }}>
+                            <Typography
+                                variant='h5'
+                                style={{ color: '#fff' }}>
+                                DevHub
+                            </Typography>
+
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center'
+                                }}>
+                                <div
+                                    className={'link-div'}
+                                    style={{
+                                        marginRight: '1rem',
+                                        padding: '0.5rem'
+                                    }}
+                                    onClick={event =>
+                                        handleAvatarClick(event)
+                                    }>
+                                    <Avatar
+                                        src={this.state.user.avatar_url}
+                                        style={{
+                                            height: '2.3rem',
+                                            width: '2.3rem'
+                                        }}>
+                                        {this.state.user.name}
+                                    </Avatar>
+                                </div>
+                                <Button
+                                    style={{ display: this.props.screenName === 'genres' ? 'none' : 'initial' }}
+                                    variant='contained'
+                                    color='secondary'
+                                    onClick={() =>
+                                        handleAddQuestionClick()
+                                    }
+                                    style={{
+                                        borderRadius: '2rem',
+                                        textTransform: 'none'
+                                    }}>
+                                    <Typography
+                                        variant='body2'
+                                        style={{
+                                            fontWeight: 600
+                                        }}>
+                                        Add Question
+                                    </Typography>
+                                </Button>
+                            </div>
+                        </div>
+                    </Container>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
