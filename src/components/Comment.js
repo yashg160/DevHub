ef81b4b30309b399cc3a78194d667cf68f14f1a0
Yash Gupta <yashg160@gmail.com>
2020-02-07 15:37:21 +0530

    Voting logic added using localStorage

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Buttons from './Buttons';
+import utils from '../utils';
+import Cookies from 'js-cookie';
 
 export default class Comment extends React.Component {
     constructor(props) {
@@ -8,9 +10,52 @@ export default class Comment extends React.Component {
         this.state = {
             answerIndex: this.props.answerIndex,
             depth: this.props.depth,
-            commentId: this.props.commentId
+            commentId: this.props.commentId,
+            parentId: this.props.parentId,
+            upvoted: false
         }
     }
+
+    componentDidMount() {
+        var upvotedComments = JSON.parse(localStorage.getItem('UPVOTED_COMMENTS'));
+
+        if (upvotedComments && upvotedComments.length > 0)
+            if (utils.checkUserInArray(upvotedComments, this.state.commentId)) this.setState({ upvoted: true });
+    }
+
+    handleClick(event) {
+        event.stopPropagation();
+        var upvote, removeUpvote;
+        if (this.state.upvoted) {
+            upvote = false;
+            removeUpvote = true;
+        }
+        else {
+            upvote = true;
+            removeUpvote = false;
+        }
+        utils.updateComment(this.props.comment, upvote, removeUpvote, this.state.commentId)
+            .then((status) => {
+                if (status === 'success') {
+                    var upvotedComments = JSON.parse(localStorage.getItem('UPVOTED_COMMENTS'));
+                    if (upvote) {
+                        console.log('Voted comment');
+                        upvotedComments.push(this.state.commentId);
+                        this.setState({ upvoted: true });
+                    }
+                    else if (removeUpvote) {
+                        console.log('Removed Upvote');
+                        utils.removeValueFromArray(upvotedComments, this.state.commentId);
+                        this.setState({ upvoted: false });
+                    }
+                    localStorage.setItem('UPVOTED_COMMENTS', JSON.stringify(upvotedComments));
+                }
+                else throw Error();
+            }).catch((error) => {
+                console.error(error);
+            });
+    }
+
     render() {
         return (
             <div style={{ marginBottom: this.props.isChild ? '0.1rem' : '0.4rem' }}>
@@ -27,7 +72,7 @@ export default class Comment extends React.Component {
                 </Typography>
                 <Typography variant='subtitle2' style={{ marginTop: '0.1rem' }}>{this.props.comment}</Typography>
                 <div style={{ display: 'flex', flexDirection: 'row', width: '25%', justifyContent: 'space-between', textTransform: 'capitalize' }}>
-                    <Buttons.UpvoteComment />
+                    <Buttons.UpvoteComment handleClick={event => this.handleClick(event)} selected={this.state.upvoted} />
                     <Buttons.ReplyComment />
                 </div>
             </div>
