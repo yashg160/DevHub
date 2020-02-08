152b69b06f1c912a59dc4e5ec4af27df4293676e
Yash Gupta <yashg160@gmail.com>
2020-02-08 21:32:02 +0530

    Completed comment

--- src/screens/tabs/UpvotedComments.js
+++ src/screens/tabs/UpvotedComments.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+
 
 export default class UpvotedComments extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			upvotedComments: props.upvotedComments
+		}
+	}
+
 	render() {
-		return <h1>This is UpvotedComments</h1>;
+		return (
+			this.state.upvotedComments.map((comment, i) => (
+				<div
+					key={i}
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						padding: '1rem',
+						border: '1px solid #eeeeee',
+						borderRadius: '4px',
+						marginBottom: '1rem'
+					}}>
+
+					<Typography
+						variant='subtitle2'
+						color='textSecondary'>
+						{comment.author_name} commented
+					</Typography>
+
+					<Typography variant='body1'>
+						{comment.comment}
+					</Typography>
+
+					<Typography
+						variant='subtitle2'
+						style={{ marginTop: '0.5rem' }}>
+						You upvoted this
+					</Typography>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							marginTop: '0.5rem',
+							verticalAlign: 'center'
+						}}>
+						<ThumbUpIcon color='primary' />
+						<Typography
+							variant='body1'
+							color='primary'
+							style={{
+								marginLeft: '4px',
+								marginRight: '4px'
+							}}>
+							&#183;
+						</Typography>
+						<Typography
+							variant='body1'
+							color='primary'
+							style={{
+								fontWeight: 600,
+								marginTop: '4px'
+							}}>
+							{comment.upvotes}
+						</Typography>
+					</div>
+
+				</div>
+			))
+		)
 	}
 }
