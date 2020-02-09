e2224139761cc9a824f26c4218b2cd6e9ec908c4
Yash Gupta <yashg160@gmail.com>
2020-02-09 18:48:25 +0530

    Replaced question modal with component

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -26,6 +26,7 @@ import Chip from '@material-ui/core/Chip';
 import Navbar from '../components/Navbar';
 import Comment from '../components/Comment';
 import MainMenu from '../components/MainMenu';
+import QuestionModal from '../components/QuestionModal';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 import serverUrl from '../config';
@@ -1051,171 +1052,23 @@ export default class Dashboard extends React.Component {
 						</div>
 					</Container>
 
-					<Modal
-						aria-labelledby='modal-question'
-						aria-describedby='modal-ask-question'
-						open={this.state.questionModal}
-						onClose={() => this.setState({ questionModal: false })}
-						closeAfterTransition
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'center',
-							alignItems: 'center'
-						}}
-						BackdropComponent={Backdrop}
-						BackdropProps={{ timeout: 500 }}>
-						<Fade in={this.state.questionModal}>
-							<div
-								style={{
-									backgroundColor: '#fff',
-									width: '50%'
-								}}>
-								<div
-									style={{
-										backgroundColor: '#e3e3e3',
-										padding: '1rem'
-									}}>
-									<Typography variant='h6'>
-										Add Question
-									</Typography>
-								</div>
-
-								<div style={{ padding: '1rem' }}>
-									<div>
-										<Typography
-											variant='h6'
-											style={{
-												fontWeight: 700,
-												marginBottom: '0.5rem'
-											}}>
-											Tips on getting good answers quickly
-										</Typography>
-
-										<div style={{ marginTop: '1rem' }}>
-											<Typography
-												variant='body1'
-												style={{ marginTop: '0.5rem' }}>
-												Make sure your question hasn't
-												been asked already
-											</Typography>
-											<Typography
-												variant='body1'
-												style={{ marginTop: '0.5rem' }}>
-												Keep your question short and to
-												the point
-											</Typography>
-											<Typography
-												variant='body1'
-												style={{ marginTop: '0.5rem' }}>
-												Double-check grammar and
-												spelling
-											</Typography>
-										</div>
-									</div>
-
-									<div
-										style={{
-											display: 'flex',
-											flexDirection: 'row',
-											justifyContent: 'flex-start',
-											marginTop: '1.5rem',
-											marginBottom: '1.5rem',
-											alignItems: 'center'
-										}}>
-										<Avatar
-											src={this.state.user.avatar_url}
-											alt={this.state.user.name}
-											style={{
-												height: '2rem',
-												width: '2rem'
-											}}
-										/>
-										<Typography
-											variant='body1'
-											style={{ marginLeft: '1rem' }}>
-											Posting as {this.state.user.name}
-										</Typography>
-									</div>
-
-									<div>
-										<TextField
-											id='question'
-											label='Your Question'
-											InputProps={{
-												style: {
-													fontSize: 26,
-													fontWeight: 600
-												}
-											}}
-											placeholder={
-												'Start you question with "What," "Why," or "How."'
-											}
-											multiline
-											rowsMax='3'
-											fullWidth
-											value={this.state.newQuestion}
-											onChange={event =>
-												this.setState({
-													newQuestion:
-														event.target.value
-												})
-											}
-											onFocus={() =>
-												this.setState({
-													newQuestionError: false
-												})
-											}
-											error={this.state.newQuestionError}
-											helperText={
-												this.state.newQuestionError
-													? 'Please check your question'
-													: ''
-											}
-										/>
-									</div>
-									<div
-										style={{
-											padding: '2rem',
-											display: 'flex',
-											flexDirection: 'row',
-											justifyContent: 'flex-end',
-											alignItems: 'center'
-										}}>
-										<Button
-											variant='text'
-											onClick={() =>
-												this.setState({
-													questionModal: false
-												})
-											}
-											style={{
-												marginRight: '0.2rem',
-												textTransform: 'none'
-											}}>
-											Cancel
-										</Button>
-										<Button
-											color='primary'
-											variant='contained'
-											onClick={() =>
-												this.setState({
-													genresModal: true,
-													questionModal: false
-												})
-											}
-											style={{
-												marginLeft: '0.2rem',
-												textTransform: 'none',
-												color: '#fff'
-											}}>
-											Proceed
-										</Button>
-									</div>
-								</div>
-							</div>
-						</Fade>
-					</Modal>
+					<QuestionModal
+						questionModal={this.state.questionModal}
+						user={this.state.user}
+						onChange={event =>
+							this.setState({ newQuestion: event.target.value })
+						}
+						newQuestionError={this.state.newQuestionError}
+						cancelClick={() =>
+							this.setState({ questionModal: false })
+						}
+						proceedClick={() =>
+							this.setState({
+								questionModal: false,
+								genresModal: true
+							})
+						}
+					/>
 					<Modal
 						aria-labelledby='modal-genres'
 						aria-describedby='modal-add-genres'
